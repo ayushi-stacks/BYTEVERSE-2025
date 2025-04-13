fc4de9f73c8412ba99c68b583c3050cde1a87491
Aftab Alam <mdalam4884@gmail.com>
2025-04-13 14:38:58 +0530

    backend finalized

--- app/admin/inventory/in-progress/page.tsx
+++ app/admin/inventory/in-progress/page.tsx
@@ -6,9 +6,15 @@ import { AdminMenudata } from "@/constants";
 import Link from "next/link";
 
 const AdminMenu = () => {
+  const showModal = true;
+
   return (
-    <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
+    <div className="relative flex h-screen max-h-screen">
+      <section
+        className={`remove-scrollbar container my-auto ${
+          showModal ? "blur-sm" : ""
+        }`}
+      >
         <div className="sub-container">
           <div className="flex justify-between">
             <Link href="/">
@@ -31,9 +37,7 @@ const AdminMenu = () => {
                 key={i}
                 className="border-px shadow-md shadow-zinc-700 bg-[#1A1D21] flex flex-col justify-between items-center h-full p-4"
               >
-                <Link
-                  href={i === 0 ? "/admin" : "/admin/inventory/in-progress"}
-                >
+                <Link href={i === 0 ? "/admin" : "/"}>
                   <CardHeader className="text-center font-semibold mt-4">
                     <CardTitle> {data.title} </CardTitle>
                   </CardHeader>
@@ -51,6 +55,31 @@ const AdminMenu = () => {
           </div>
         </div>
       </section>
+
+      {showModal && (
+        <div className="fixed inset-0  flex items-center justify-center z-50">
+          <div className="bg-[#D2D2D2] rounded-lg p-8 text-center relative w-full max-w-[500px] h-[280px] mx-auto">
+            <Link href={`/admin-menu`}>
+              <Icon
+                icon="mdi:arrow-left"
+                width={30}
+                height={30}
+                className="absolute top-4 text-black left-4 cursor-pointer"
+              />
+            </Link>
+            <h2 className="text-2xl text-zinc-800 mt-6 font-bold mb-8">
+              The inventory stock will be updated soon after evaluations are
+              over.
+            </h2>
+            <Icon
+              icon="carbon:warning"
+              width={50}
+              height={50}
+              className="text-black mx-auto"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
